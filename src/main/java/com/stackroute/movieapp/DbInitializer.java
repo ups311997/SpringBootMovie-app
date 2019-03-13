d1942ce5c45b18c670fc054946785f62dd93ab46
ups311997 <upasanapoddar3@gmail.com>
2019-03-13 10:45:46 +0530

    ninth commit

--- src/main/java/com/stackroute/movieapp/DbInitializer.java
+++ src/main/java/com/stackroute/movieapp/DbInitializer.java
@@ -1,13 +1,21 @@
 package com.stackroute.movieapp;
 
 import com.stackroute.movieapp.domain.Movie;
+import com.stackroute.movieapp.exceptions.MovieAlreadyExistsException;
+import com.stackroute.movieapp.handler.GlobalExceptionHandler;
 import com.stackroute.movieapp.repository.MovieRepository;
+import com.stackroute.movieapp.service.MovieService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
 @Component
+@PropertySource("classpath:application.properties")
 public class DbInitializer implements CommandLineRunner, ApplicationListener<ContextRefreshedEvent> {
     private MovieRepository movieRepository;
 
@@ -25,13 +33,28 @@ public class DbInitializer implements CommandLineRunner, ApplicationListener<Con
         this.movieRepository.save(movie2);
     }
 
+    @Autowired
+    private MovieService movieService;
+
+    @Autowired
+    Environment env;
+
+    @Value("${id}")
+    int id;
+
+    @Value("${details}")
+    String details;
+
+
+
     @Override
     public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
 
-        Movie movie1=new Movie(1,"Annabelle","Horror");
-        Movie movie2=new Movie(2,"Conjuring2","Super-Horror");
+        try {
+            movieService.saveMovie(new Movie(id,env.getProperty("name"),details));
+        } catch (MovieAlreadyExistsException e) {
+            e.printStackTrace();
+        }
 
-        this.movieRepository.save(movie1);
-        this.movieRepository.save(movie2);
     }
 }
