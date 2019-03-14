54e6fa8ba96073d2730ab55e59edd3e698463825
ups311997 <46953880+ups311997@users.noreply.github.com>
2019-03-14 12:00:51 +0530

    Update Movie.java

--- src/main/java/com/stackroute/movieapp/domain/Movie.java
+++ src/main/java/com/stackroute/movieapp/domain/Movie.java
@@ -20,38 +20,6 @@ public class Movie {
     private int movieid;
     private String moviename;
     private String moviedetails;
-//
-//    public Movie() {
-//    }
-//
-//    public Movie(String moviename, String moviedetails) {
-//
-//        this.moviename = moviename;
-//        this.moviedetails = moviedetails;
-//    }
-//
-//    public int getMovieid() {
-//        return movieid;
-//    }
-//
-//    public void setMovieid(int movieid) {
-//        this.movieid = movieid;
-//    }
-//
-//    public String getMoviename() {
-//        return moviename;
-//    }
-//
-//    public void setMoviename(String moviename) {
-//        this.moviename = moviename;
-//    }
-//
-//    public String getMoviedetails() {
-//        return moviedetails;
-//    }
-//
-//    public void setMoviedetails(String moviedetails) {
-//        this.moviedetails = moviedetails;
-//    }
+
 }
 
